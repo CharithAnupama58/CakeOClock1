e8ab7aa68d0e5a38119e1be983482c1ce61598a1
CharithAnupama58 <charith20anupama@gmail.com>
2024-04-07 12:25:08 +0530

    Update1

--- Server/Controller/stockManagementController.js
+++ Server/Controller/stockManagementController.js
@@ -2,7 +2,6 @@ import {db} from '../server.js'
 
 export const getItemDetails = async (req, res) => {
     try {
-        // Query to retrieve all item details from the database
         const items = await new Promise((resolve, reject) => {
             db.query('SELECT s.*, i.* FROM Stock s JOIN Item i ON s.ItemId = i.ItemId WHERE s.ExpiryDate >= CURDATE() ORDER BY s.ExpiryDate ASC', (error, results) => {
                 if (error) {
@@ -12,8 +11,6 @@ export const getItemDetails = async (req, res) => {
                 }
             });
         });
-
-        // Check if any items were found
         if (items.length > 0) {
             return res.status(200).json({ items });
         } else {
@@ -24,3 +21,120 @@ export const getItemDetails = async (req, res) => {
         return res.status(500).json({ error: 'Internal Server Error' });
     }
 };
+
+export const getItemIds = async (req, res) => {
+    try {
+        const options = await new Promise((resolve, reject) => {
+            db.query('SELECT itemName FROM item', (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+
+        if (options.length > 0) {
+            return res.status(200).json({ options });
+        } else {
+            return res.status(404).json({ error: 'No items found' });
+        }
+    } catch (error) {
+        console.log('Error fetching items:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+export const getItemNameDetails = async (req, res) => {
+    const itemName = req.params.selectedOption;
+    console.log(req.params.selectedOption);
+    console.log(req.params);
+    // console.log(itemName);
+    try {
+        // Query to retrieve all item details from the database
+        const itemNameDetails = await new Promise((resolve, reject) => {
+            db.query('SELECT * FROM item WHERE itemName = ?', [itemName], (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+        console.log(itemNameDetails)
+
+        if (itemNameDetails.length > 0) {
+            return res.status(200).json({ itemNameDetails });
+        } else {
+            return res.status(404).json({ error: 'No items found' });
+        }
+    } catch (error) {
+        console.log('Error fetching items:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+export const saveStockDetails = async (req, res) => {
+    const { ItemId, ExpiryDate } = req.body;
+    let Quantity=parseInt(req.body.Quantity);
+    console.log(req.body);
+    
+
+    if (!ItemId || !Quantity || !ExpiryDate) {
+        return res.status(400).json({ error: 'ItemId, Quantity, and ExpiryDate are required' });
+    }
+
+    try {
+        const existingRecord = await new Promise((resolve, reject) => {
+            db.query('SELECT * FROM Stock WHERE ItemId = ? AND ExpiryDate = ?', [ItemId, ExpiryDate], (error, result) => {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            });
+        });
+
+        if (!existingRecord || existingRecord.length === 0) {
+            const insertResult = await new Promise((resolve, reject) => {
+                db.query('INSERT INTO Stock (ItemId, Quantity, ExpiryDate) VALUES (?, ?, ?)', [ItemId, Quantity, ExpiryDate], (error, result) => {
+                    if (error) {
+                        reject(error);
+                    }
+                    resolve(result);
+                });
+            });
+
+            if (insertResult.affectedRows >= 1) {
+                return res.status(200).json({ message: 'Stock details added successfully' });
+            } else {
+                return res.status(500).json({ error: 'Failed to add stock details' });
+            }
+        }else if (existingRecord.length > 0){
+            let oldQty=existingRecord[0].Quantity;
+            let newQty =Quantity + oldQty;
+            Quantity=newQty;
+            const updateResult = await new Promise((resolve, reject) => {
+                db.query('UPDATE Stock SET Quantity = ? WHERE ItemId = ? AND ExpiryDate = ?', [Quantity, ItemId, ExpiryDate], (error, result) => {
+                    if (error) {
+                        reject(error);
+                    }
+                    resolve(result);
+                });
+            });
+
+            if (updateResult.affectedRows >= 1) {
+                return res.status(200).json({ message: 'Stock details updated successfully' });
+            } else {
+                return res.status(500).json({ error: 'Failed to update stock details' });
+            }
+
+        }
+        
+    } catch (error) {
+        console.log('Error saving stock details:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+
+
--- Server/Routes/stockManagement.js
+++ Server/Routes/stockManagement.js
@@ -1,11 +1,20 @@
 import express from 'express';
-import { getItemDetails } from '../Controller/stockManagementController.js';
+import { getItemDetails,getItemIds,getItemNameDetails,saveStockDetails } from '../Controller/stockManagementController.js';
 
 const router = express.Router();
 
 router.get('/itemDetails', async (req, res) => {
     await getItemDetails(req, res);
 });
+router.get('/itemIds', async (req, res) => {
+    await getItemIds(req, res);
+});
 
+router.get('/itemNameDetails/:selectedOption', async (req, res) => {
+    await getItemNameDetails(req, res);
+});
+router.post('/addStock', async (req, res) => {
+    await saveStockDetails(req, res);
+});
 
 export default router;
